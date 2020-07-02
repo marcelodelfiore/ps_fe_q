5e9a65b96b254f267f2e323a0bae25c4f4210820
Marcelo Del Fiore <marcelodelfiore@gmail.com>
2020-07-02 16:40:05 -0300

    Layout improvements @ recipe page

--- src/pages/recipes/recipe.vue
+++ src/pages/recipes/recipe.vue
@@ -3,45 +3,52 @@
 
     <div class="q-pa-md">
       <div class="row">
-        <div class="col-6 text-h4 text-bold text-center">
-          {{ recipeByID($route.params.id).title }}  
+        <div class="col-12 text-h4 text-bold text-center fill-height">
+          {{ recipeByID($route.params.id).title }}
         </div>
-
-        <div class="col-6 text-h3 text-bold text-center">
-          <q-btn round color="primary" icon="build" class="q-mt-md" @click="newBatch">
+      </div>
+      <div class="row">
+        <q-space/>
+        <div class="col-4 vertical-middle text-center">
+          <q-btn round color="primary" icon="build" class="q-mt-sm q-mb-lg" @click="newBatch">
             <q-tooltip >Criar novo lote de produto</q-tooltip>
-          </q-btn>  
+          </q-btn>
         </div>
 
       </div>
       
-      <div class="q-pa-md row items-start q-gutter-md">
-        <div class="q-pa-sm col-2 col-md-2 text-center" v-for="(ingredient, index) in recipeByID($route.params.id).ingredients"
+      <div class="row">
+        <div class="col-xs-12 col-md-4 col-lg-3 text-center text-h4" v-for="(ingredient, index) in recipeByID($route.params.id).ingredients"
             :key="index">         
-              <q-card class="no-shadow">
-                <q-card-section>
-                  <q-img :src= "(rawMaterialByID(ingredient.id)).thumbnail"
-                  style="height: 75px; max-width: 75px"
-                  />
-                </q-card-section>
-                <q-card-section>
-                  <div class="text-h6">{{ (rawMaterialByID(ingredient.id)).description }} </div>
-                  <div class="text-subtitle2">{{ (textureByID(ingredient.texture)).description }}</div>
-                </q-card-section>
+          <q-card class="no-shadow">
+            <q-card-section>
+              <q-img :src= "(rawMaterialByID(ingredient.id)).thumbnail"
+              style="height: 150px; max-width: 150px"
+              />
+            </q-card-section>
+            <q-card-section>
+              <div class="text-h6">{{ (rawMaterialByID(ingredient.id)).description }} </div>
+              <div class="text-h6">{{ (textureByID(ingredient.texture)).description }}</div>
+            </q-card-section>
 
-                <q-card-section class="q-pt-none">
-                  {{ ingredient.percent }}%    
-                </q-card-section>
-              </q-card>
+            <q-card-section class="q-pt-none">
+              {{ ingredient.percent }}%    
+            </q-card-section>
+          </q-card>
         </div>
-
 
         <div class="q-pa-sm col-12 text-left text-h5" v-for="step in recipeByID($route.params.id).prep_steps"
           :key="step.seq">
-            <strong>{{ step.seq }}</strong>
-            {{ step.description }}
+          <div class="row">
+            <div class="col-1">
+              <q-icon name="directions_run"/>
+            </div>
+            
+            <div class="col">
+              {{ step.description }}
+            </div>
+          </div>              
         </div>
-      
       </div>
     </div>
 
