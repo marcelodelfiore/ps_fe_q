577addabe991420a8f4f38b3ec0683d60f4ed784
Marcelo Del Fiore <marcelodelfiore@gmail.com>
2020-07-24 14:37:43 -0300

    Created edit page & its basic layout

---
+++ src/pages/recipes/editRecipe.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="q-ma-md">
+        <div class="row justify-around align-items:center">
+            <div class="col q-pa-md text-center text-h4 text-bold">
+                Editar Receita - {{ recipeToSubmit.recipe.title }}
+            </div>
+        </div>
+        
+        <div class="row">
+            <div class="col q-pa-md justify-around">
+                form dos dados de cadastro
+            </div>
+
+            <div class="col q-pa-md justify-around">
+                form dos dados de ingredientes
+            </div>
+
+            <div class="col q-pa-md justify-around">
+                {{ recipeToSubmit }}
+            </div>
+
+        </div>
+
+    </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+    data() {
+			return {
+                recipeToSubmit: {}
+			}
+	},
+    computed:{
+        ...mapGetters('recipes', ['recipeByID'])
+    },
+    mounted() {
+      this.recipeToSubmit = Object.assign({}, this.recipeByID(this.$route.params.id))
+    }
+}
+</script>
--- src/store/recipes/mutations.js
+++ src/store/recipes/mutations.js
@@ -2,14 +2,11 @@ import Vue from 'vue'
 
 export function addNewRecipe(state, payload){
     state.listOfRecipes.push(payload)
-    // Vue.set(state.listOfRecipes, payload.id, payload.recipe)
 }
 
 export function deleteRecipe(state, id){
     const index = state.listOfRecipes.findIndex(r => r.id === id)
-    console.log('Na mutation deleteRecipe index=',index)
     if(index > -1){
         state.listOfRecipes.splice(index,1)
     }
-    //Vue.delete(state.listOfRecipes, id)
 }
