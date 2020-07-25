cd86dc3c3b99d94237f90aa3c6bae696304fa3ba
Marcelo Del Fiore <marcelodelfiore@gmail.com>
2020-07-25 12:48:48 -0300

    Use only one form for all recipe info

--- src/pages/recipes/editRecipe.vue
+++ src/pages/recipes/editRecipe.vue
@@ -6,21 +6,10 @@
             </div>
         </div>
         
-        <div class="row">
-            <div class="col q-pa-md justify-around">
-                <editRecipeFormData 
-                    :recipeToEdit="recipeByID(this.$route.params.id).recipe"
-                />
-            </div>
-
-            <div class="col q-pa-md justify-around">
-                <editRecipeFormIngredients :ingredientsToEdit="this.recipeToSubmit" />
-            </div>
-
-            <div class="col q-pa-md justify-around">
-                <editRecipeFormPrepSteps :prepStepsToEdit="this.recipeByID" />
-            </div>
-
+        <div class="col q-pa-md justify-around">
+            <editRecipeForm 
+                :recipeToEdit="recipeByID(this.$route.params.id).recipe"
+            />
         </div>
 
     </div>
@@ -29,9 +18,7 @@
 <script>
 import { mapGetters } from 'vuex'
 
-import editRecipeFormData from 'components/recipes/editRecipeFormData'
-import editRecipeFormIngredients from 'components/recipes/editRecipeFormIngredients'
-import editRecipeFormPrepSteps from 'components/recipes/editRecipeFormPrepSteps'
+import editRecipeForm from 'components/recipes/editRecipeForm'
 
 export default {
     data() {
@@ -43,9 +30,7 @@ export default {
         ...mapGetters('recipes', ['recipeByID'])
     },
     components: {
-        editRecipeFormData,
-        editRecipeFormIngredients,
-        editRecipeFormPrepSteps
+        editRecipeForm
     }
 }
 </script>
--- src/pages/recipes/recipe.vue
+++ src/pages/recipes/recipe.vue
@@ -37,7 +37,7 @@
       <div class="row">
         <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 text-center text-h4"
          v-for="ingredient in recipeByID($route.params.id).recipe.ingredients"
-         :key="ingredient.sequence">         
+         :key="ingredient.sequence">
           <q-card class="no-shadow">
             <q-card-section>
               <q-avatar size="150px">
