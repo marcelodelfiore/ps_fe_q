b2afe9d19f5ad25aeefefa697e5e70625f31c28d
Marcelo Del Fiore <marcelodelfiore@gmail.com>
2020-07-30 09:44:18 -0300

    Update layout to Facebook login button

--- src/pages/auth/auth.vue
+++ src/pages/auth/auth.vue
@@ -1,54 +1,66 @@
 <template>
 	<q-page padding>
+		<div class="row">
+			<div class="col-5">
 
-		<q-btn @click="facebookLogin"/>
+			</div>
+			<div v-if="!userLoggedIn" class="col text-white fixed-center" style="background-color: #3b5998">
+				<q-btn @click="facebookLogin" align="around" icon="img:icons/facebook.svg">Entrar com Facebook</q-btn>
+			</div>
+		
+			<div class="col-5">
+
+			</div>
+		</div>
+		
 
 	</q-page>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 import * as firebase from "firebase"
 
 import "firebase/auth"
-import { firebaseAuth } from '../../boot/firebase'
 
-	export default {
-		data () {
-	    return {
-	      tab: 'login'
-	    }
-	  },
-	  components: {
-	  },
-	  methods:{
-		  facebookLogin: function(){
-			var provider = new firebase.auth.FacebookAuthProvider();
-			//var provider = new firebase.auth().FacebookAuthProvider();
-			provider.addScope('user_birthday');
-			firebaseAuth.signInWithPopup(provider)
-			.then(function(result) {
+export default {
+	data () {
+		return {
+			tab: 'login'
+		}
+	},
+	computed: {
+		...mapState('auth', ['userLoggedIn']),
+	},
+	methods:{
+		facebookLogin: function(){
+		var provider = new firebase.auth.FacebookAuthProvider();
+		provider.addScope('user_birthday');
+		firebase.auth().signInWithRedirect(provider);
+		firebase.auth().getRedirectResult()
+		.then(function(result) {
+			if (result.credential) {
 				// This gives you a Facebook Access Token. You can use it to access the Facebook API.
 				var token = result.credential.accessToken;
-				// The signed-in user info.
-				var user = result.user;
 				// ...
-				console.log('facebook token: ', token)
-				console.log('facebook user: ', user)
-			})
-			.catch(function(error) {
-				// Handle Errors here.
-				var errorCode = error.code;
-				var errorMessage = error.message;
-				// The email of the user's account used.
-				var email = error.email;
-				// The firebase.auth.AuthCredential type that was used.
-				var credential = error.credential;
-				// ...
-				console.log('facebook error: ', error)
-			});
-		  }
-	  }
+			}
+			// The signed-in user info.
+			var user = result.user;
+		})
+		.catch(function(error) {
+			// Handle Errors here.
+			var errorCode = error.code;
+			var errorMessage = error.message;
+			// The email of the user's account used.
+			var email = error.email;
+			// The firebase.auth.AuthCredential type that was used.
+			var credential = error.credential;
+			// ...
+		});
+		}
 	}
+}
 </script>
 
 <style>
