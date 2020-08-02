a3ca7e61ecf5729628d22c9e47aae882c99ea6e8
Marcelo Del Fiore <marcelodelfiore@gmail.com>
2020-08-02 11:13:54 -0300

    Include thumbnail to raw specific page and update some of the pictures

--- src/pages/raws/raw.vue
+++ src/pages/raws/raw.vue
@@ -3,6 +3,11 @@
     Specific Raw Materials Page !!!
     <h3>Página de matéria prima específica {{ $route.params.id }}</h3>
     {{ rawMaterialByID($route.params.id).description }}
+    <q-img
+      :src="rawMaterialByID($route.params.id).thumbnail"
+      spinner-color="white"
+      style="height: 140px; max-width: 150px"
+    />
 
   </q-page>
 </template>
